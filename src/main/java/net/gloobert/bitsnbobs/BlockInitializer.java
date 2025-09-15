ad053454c0da931cd1fba692fda0d175806d548e
Mrrrp Mreow :3 <mrrrpmreow@gmail.com>
2025-09-15 15:42:39 -0230

    Wall.

--- src/main/java/net/gloobert/bitsnbobs/BlockInitializer.java
+++ src/main/java/net/gloobert/bitsnbobs/BlockInitializer.java
@@ -15,7 +15,7 @@ import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
 public class BlockInitializer {
 
     public static final Block BLUE_NETHER_BRICKS = register("blue_nether_bricks", new Block(Block.Settings.create().strength(4.0f)), true);
-    public static final Block BLUE_NETHER_BRICK_WALL = register("blue_nether_brick_wall", new Block(Block.Settings.create().strength(4.0f)), true);
+    public static final Block BLUE_NETHER_BRICK_WALL = register("blue_nether_brick_wall", new WallBlock(Block.Settings.create().strength(4.0f)), true);
     public static final Block POLISHED_TILES = register("polished_tiles", new Block(Block.Settings.create().strength(4.0f)), true);
     public static final Block PURIFIED_CORRUPTION = register("purified_corruption", new Block(Block.Settings.create().strength(4.0f)), true);
     public static final Block REFINED_CORRUPTION = register("refined_corruption", new Block(Block.Settings.create().strength(4.0f)), true);

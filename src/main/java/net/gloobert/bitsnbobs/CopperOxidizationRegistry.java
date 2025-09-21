b2c3ccaa6928c05e893d37a8f1c99d50ad875bb0
Mrrrp Mreow :3 <mrrrpmreow@gmail.com>
2025-09-21 00:40:22 -0230

    oops. that needs to be in the function.

--- src/main/java/net/gloobert/bitsnbobs/CopperOxidizationRegistry.java
+++ src/main/java/net/gloobert/bitsnbobs/CopperOxidizationRegistry.java
@@ -4,6 +4,8 @@ import net.fabricmc.fabric.api.registry.OxidizableBlocksRegistry;
 import net.gloobert.bitsnbobs.BlockInitializer;
 
 public class CopperOxidizationRegistry {
+
+    public static void initialize() {
     OxidizableBlocksRegistry.registerOxidizableBlockPair(BlockInitializer.SANDED_COPPER_BLOCK, BlockInitializer.SANDED_EXPOSED_COPPER_BLOCK);
     OxidizableBlocksRegistry.registerOxidizableBlockPair(BlockInitializer.SANDED_EXPOSED_COPPER_BLOCK, BlockInitializer.SANDED_WEATHERED_COPPER_BLOCK);
     OxidizableBlocksRegistry.registerOxidizableBlockPair(BlockInitializer.SANDED_WEATHERED_COPPER_BLOCK, BlockInitializer.SANDED_OXIDIZED_COPPER_BLOCK);
@@ -12,8 +14,5 @@ public class CopperOxidizationRegistry {
     OxidizableBlocksRegistry.registerWaxableBlockPair(BlockInitializer.SANDED_EXPOSED_COPPER_BLOCK, BlockInitializer.WAXED_SANDED_EXPOSED_COPPER_BLOCK);
     OxidizableBlocksRegistry.registerWaxableBlockPair(BlockInitializer.SANDED_WEATHERED_COPPER_BLOCK, BlockInitializer.WAXED_SANDED_WEATHERED_COPPER_BLOCK);
     OxidizableBlocksRegistry.registerWaxableBlockPair(BlockInitializer.SANDED_OXIDIZED_COPPER_BLOCK, BlockInitializer.WAXED_SANDED_OXIDIZED_COPPER_BLOCK);
-
-
-    public static void initialize() {
     }
 }
